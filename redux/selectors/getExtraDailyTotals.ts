bef3bb073a79ba971c2cbc369ed9fa74dc08fedd
Yuho <yuho@ref.finance>
2023-12-29 20:05:02 +0800

    log

--- redux/selectors/getExtraDailyTotals.ts
+++ redux/selectors/getExtraDailyTotals.ts
@@ -11,15 +11,25 @@ export const getExtraDailyTotals = ({ isStaking = false }: { isStaking: boolean 
     (state: RootState) => state.app.config,
     (assets, rewards, config) => {
       if (!hasAssets(assets)) return 0;
-
+      if (!isStaking) {
+        console.info(`=====getExtraDailyTotals1=====`);
+      }
       const { extra, brrr } = rewards;
-
       const gainBrrr =
         (brrr.dailyAmount || 0) * (assets.data[config.booster_token_id]?.price?.usd || 0);
-
+      if (!isStaking) {
+        console.info(
+          `token:${config.booster_token_id} gainBrrr:${gainBrrr}=> daily:${
+            brrr.dailyAmount
+          }*price:${assets.data[config.booster_token_id]?.price?.usd}`,
+        );
+      }
       const gainExtra = Object.keys(extra).reduce((acc, tokenId) => {
         const price = assets.data[tokenId]?.price?.usd || 0;
         const daily = isStaking ? extra[tokenId].newDailyAmount : extra[tokenId].dailyAmount;
+        if (!isStaking) {
+          console.info(`token:${tokenId} acc:${acc}=> daily:${daily}*price:${price}`);
+        }
         return acc + daily * price;
       }, 0);
 
