e04079cd448ac0c6c958ecbcf2d5731cd0f1fa9c
xieqian <dss@dsss-MacBook-Pro.local>
2024-04-24 11:24:06 +0800

    add brrr.ft.ref-labs.testnet price

--- utils/config.ts
+++ utils/config.ts
@@ -17,11 +17,16 @@ export const BRRR_TOKEN = {
   testnet: "test_brrr.1638481328.burrow.testnet",
   mainnet: "token.burrow.near",
 };
+// for pubtestnet env
+export const BRRR_LABS_TOKEN = {
+  testnet: "brrr.ft.ref-labs.testnet",
+  mainnet: "brrr.ft.ref-labs.testnet",
+};
 
 export const WALLET_CONNECT_ID =
   process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || ("87e549918631f833447b56c15354e450" as string);
 
-export const missingPriceTokens = [REF_TOKEN, META_TOKEN, BRRR_TOKEN];
+export const missingPriceTokens = [REF_TOKEN, META_TOKEN, BRRR_TOKEN, BRRR_LABS_TOKEN];
 export const incentiveTokens = [
   "853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near",
   "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
