962901b4f0ef31857be2f39f6e99c720ff5f984a
lq0-github <1441665200@qq.com>
2024-05-24 16:45:31 +0800

    add tx pikespeakl

--- components/CustomTable/CustomTable.tsx
+++ components/CustomTable/CustomTable.tsx
@@ -228,9 +228,6 @@ const StyledTable = styled.div`
   .custom-table-td {
     flex: 1;
     word-break: break-word;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
   }
 
   .custom-table-thead {
